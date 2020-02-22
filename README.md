2088f94e53400d02b033c197c954d4d606dddd7b
YYY <yyy3752@gmail.com>
2020-02-22 23:31:58 +0900

    improvements

--- README.md
+++ README.md
@@ -2,22 +2,13 @@
 
 通过LaunchHelper的Jar可以实现不用添加任何命令行参数载入Java agent并启动另一个可执行Jar，用于在Multicraft面板服使用[authlib-injector](https://github.com/yushijinhun/authlib-injector)。
 
-## 构建
+## 使用方法
 
-可以用Maven构建，环境要求：
+1. 从[Release](https://github.com/Codex-in-somnio/LaunchHelper/releases)获取`LaunchHelper-{版本}.jar`，如果不确定用Windows还是Linux的版本，可以先尝试Linux版本，出错再尝试Windows版本（详见下面的注意事项）；
 
-* Java JDK 1.8
-* Maven
+2. 将`LaunchHelper-{版本}.jar`放在Minecraft服务端根目录；
 
-```
-mvn clean package
-```
-
-## 使用
-
-1. 将LaunchHelper的Jar以及`lib`文件夹一起放在Minecraft服务端根目录
-
-2. 创建一个命名为`launchhelper.properties`的文件，用于LaunchHelper的配置，填入：
+3. 创建一个命名为`launchhelper.properties`的文件，用于LaunchHelper的配置，填入：
 
    ```
    javaAgentJarPath=<authlib-injector的Jar路径>
@@ -28,16 +19,32 @@ mvn clean package
    例如：
 
    ```
-   javaAgentJarPath=authlib-injector-1.1.26-41a7a47.jar
+   javaAgentJarPath=authlib-injector.jar
    javaAgentOptions=https://example.com/api/yggdrasil/
-   execJarPath=paper-105.jar
+   execJarPath=paper.jar
    ```
 
-3. 从LaunchHelper的Jar启动，例如将LaunchHelper的Jar重命名成自定义服务端需要的特定的文件名，具体需参考面板服方面的说明或咨询面板服客服。
+   也可以直接先启动一次（参考下一步），使样例配置文件自动生成；
+
+4. 在面板服上指定由`LaunchHelper-{版本}.jar`启动，或者重命名成自定义服务端需要的特定的文件名，具体需参考面板服方面的说明或咨询面板服客服。
 
 ## 注意事项
 
-如果出现`java.lang.UnsatisfiedLinkError`的异常：
+* LaunchHelper不能跨平台使用，需要使用和平台对应的Jar，跨平台会出现`java.lang.UnsatisfiedLinkError`；
+* 如果在Windows环境下用JRE运行，相关动态链接库（`attach.dll`）需要确保能被链接到，经测试发现Windows环境下JRE可能不包含此DLL；遇到这种情况时会出现`no providers installed`的错误消息，可以从JDK安装目录下找到`jre/bin/attach.dll`复制一份到工作目录（服务端根目录）然后启动；
+* 目前只测试过Java 8，只有针对Java 8的版本，其他Java版本不保证可用。
 
-* 依赖的`tools.jar`不能跨平台使用，要确保`tools.jar`是来自同一平台的；
-* 如果用JRE运行，相关动态链接库需要能被链接到。
+## 从源码构建
+
+可以用Maven构建，环境要求：
+
+- Java JDK 1.8
+- Maven
+
+执行以下命令构建：
+
+```
+mvn clean package
+```
+
+构建结果位于`target/LaunchHelper-{版本}.jar`
--- src/main/java/cx/y3/mc/LaunchHelper/Main.java
+++ src/main/java/cx/y3/mc/LaunchHelper/Main.java
@@ -5,20 +5,57 @@ import com.sun.tools.attach.VirtualMachine;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		Properties props = readPropertiesFile("launchhelper.properties");
-		attachJavaAgent(props.getProperty("javaAgentJarPath"), props.getProperty("javaAgentOptions"));
-		runJar(props.getProperty("execJarPath"));
+
+	public static String agentJarPath;
+	public static String agentOptions;
+	public static String execJarPath;
+
+	public static void main(String[] args) {
+		printSplash();
+		String propsFileName = "launchhelper.properties";
+		File propsFile = new File(propsFileName);
+
+		if (!propsFile.exists()) {
+			log("Properties file " + propsFileName + " does not exist");
+			InputStream examplePropsFileIs = Main.class.getResourceAsStream("/" + propsFileName);
+			try {
+				Files.copy(examplePropsFileIs, Paths.get(propsFileName));
+			} catch (IOException e) {
+				log("Failed to generate " + propsFileName);
+				log(e.getLocalizedMessage());
+				return;
+			}
+			log("A sample version has been created");
+			log("Please modify it as needed.");
+			return;
+		}
+
+		try {
+			Properties props = readPropertiesFile(propsFileName);
+			agentJarPath = props.getProperty("javaAgentJarPath");
+			agentOptions = props.getProperty("javaAgentOptions");
+			execJarPath = props.getProperty("execJarPath");
+		} catch (IOException e) {
+			log("Failed to read " + propsFileName);
+			log(e.getLocalizedMessage());
+			return;
+		}
+		if (!runJar()) {
+			log("Failed to launch");
+		}
 	}
 
 	public static Properties readPropertiesFile(String fileName) throws IOException {
@@ -31,32 +68,116 @@ public class Main {
 		return prop;
 	}
 
-	public static void attachJavaAgent(String jarPath, String options) throws Exception {
+	public static boolean attachJavaAgent() {
+		log(String.format("Loading Java agent %s=%s", agentJarPath, agentOptions));
 		String pid = ManagementFactory.getRuntimeMXBean().getName().split("@")[0];
-		VirtualMachine vm = VirtualMachine.attach(pid);
-		vm.loadAgent(jarPath, options);
-		vm.detach();
+		VirtualMachine vm;
+		try {
+			vm = VirtualMachine.attach(pid);
+		} catch (Exception e) {
+			log("Failed to attach to the JVM");
+			log(e.getLocalizedMessage());
+			return false;
+		}
+		try {
+			vm.loadAgent(agentJarPath, agentOptions);
+			vm.detach();
+		} catch (Exception e) {
+			log(e.getLocalizedMessage());
+			return false;
+		}
+		return true;
 	}
 
-	public static void runJar(String jarPath) throws Exception {
-		JarFile jarFile = new JarFile(jarPath);
-		Manifest manifest = jarFile.getManifest();
-		Attributes attributes = manifest.getMainAttributes();
-		String className = attributes.getValue(Attributes.Name.MAIN_CLASS);
-		jarFile.close();
+	public static boolean runJar() {
+		log("Loading executable Jar " + execJarPath);
+		JarFile jarFile;
+		try {
+			jarFile = new JarFile(execJarPath);
+		} catch (IOException e) {
+			log("Failed to open the specified executable jar");
+			log(e.getLocalizedMessage());
+			return false;
+		}
+		Manifest manifest;
+		Attributes attributes;
+		String className;
+		try {
+			manifest = jarFile.getManifest();
+			attributes = manifest.getMainAttributes();
+			className = attributes.getValue(Attributes.Name.MAIN_CLASS);
+			jarFile.close();
+		} catch (IOException e) {
+			log("Failed to get manifest from the specified executable jar");
+			log(e.getLocalizedMessage());
+			return false;
+		}
 
-		File file = new File(jarPath);
-		URL[] urls = { file.toURI().toURL() };
-		URLClassLoader loader = new URLClassLoader(urls);
-		Class<?> cls = loader.loadClass(className);
-		Method mcMain = cls.getDeclaredMethod("main", String[].class);
-		String[] mcArgs = {};
+		File file = new File(execJarPath);
+		try {
+			URL[] urls = { file.toURI().toURL() };
+			URLClassLoader loader = new URLClassLoader(urls);
+			Class<?> cls = loader.loadClass(className);
+			Method mcMain = cls.getDeclaredMethod("main", String[].class);
+			String[] mcArgs = {};
 
-		ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
-		Thread.currentThread().setContextClassLoader(cls.getClassLoader());
-		mcMain.invoke(null, (Object) mcArgs);
-		Thread.currentThread().setContextClassLoader(oldCl);
+			ClassLoader oldCl = Thread.currentThread().getContextClassLoader();
+			Thread.currentThread().setContextClassLoader(cls.getClassLoader());
 
-		loader.close();
+			if (!attachJavaAgent()) {
+				log("Failed to attach Java agent");
+				loader.close();
+				return false;
+			}
+			mcMain.invoke(null, (Object) mcArgs);
+
+			Thread.currentThread().setContextClassLoader(oldCl);
+
+			loader.close();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		return true;
+	}
+
+	public static void printSplash() {
+		log("");
+		log("    //       ////   //  //  //  //   /////  //  //");
+		log("   //      //  //  //  //  /// //  //      //  //  ___");
+		log("  //      //////  //  //  // ///  //      //////  ____");
+		log(" //      //  //  //  //  //  //  //      //  //  _____");
+		log("//////  //  //   ////   //  //   /////  //  //  ______");
+		log("");
+		log("      //  //  //////  //      /////   //////  /////");
+		log("     //  //  //      //      //  //  //      //  //  ___");
+		log("    //////  /////   //      /////   /////   /////   ____");
+		log("   //  //  //      //      //      //      //  //  _____");
+		log("  //  //  //////  //////  //      //////  //  //  ______");
+		log("");
+
+		Properties buildInfo = new Properties();
+		try {
+			InputStream biif = Main.class.getResourceAsStream("/META-INF/build-info.properties");
+			if (biif == null)
+				throw new IOException();
+			buildInfo.load(biif);
+		} catch (IOException e) {
+			log("Failed to read build information: " + e.getLocalizedMessage());
+		}
+		String name = "LaunchHelper";
+		String version = buildInfo.getProperty("build.version");
+		String info = String.format("%s (%s) Created with ♥ by Codex in Somnio", name, version);
+		String rule = "";
+		for (int i = 0; i < info.length(); ++i)
+			rule += "=";
+		log(rule);
+		log(info);
+		log("https://github.com/Codex-in-somnio/LaunchHelper");
+		log(rule);
+		log("");
+	}
+
+	public static void log(String message) {
+		System.out.println("[LH] " + message);
 	}
 }
